d320de72baf75692ec73c57880aeadc5aba75729
Richard M Fogarty <richard.m.fogarty@gmail.com>
2019-07-06 12:22:58 +0100

    initial commit

---
+++ utest_stuff.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import unittest
+
+
+def addTwoNumbers(x,y):
+	return 8
+
+
+class testAddTwoNumbers(unittest.TestCase):
+	def testFailingTest(self):
+		self.assertTrue(False)
+
+
+if __name__ == '__main__':
+	unittest.main()
+
